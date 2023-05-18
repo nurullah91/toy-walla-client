befd5eabd9cbb5c746841bb0ddcfb0d5dd2f6541
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-18 10:30:18 +0600

    tailwind and daisy us setup done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <>
       <h1>Welcome to Toy-Wala</h1>
+      <h1 className="text-3xl btn btn-primary font-bold underline">
+      Hello world!
+    </h1>
       
     </>
   )
