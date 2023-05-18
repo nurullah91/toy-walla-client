05fbed3c203c9a4c3b2f4a6ab43c6d5f742f5df5
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-18 11:03:47 +0600

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Navbar from './Pages/Shared/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       <h1>Welcome to Toy-Wala</h1>
       <h1 className="text-3xl btn btn-primary font-bold underline">
       Hello world!
