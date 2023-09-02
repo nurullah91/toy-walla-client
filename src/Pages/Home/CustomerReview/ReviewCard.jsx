a26de9e205cbdbb00eef15824db6a274f6d83150
nurullah91 <mdnurullah.ptk@gmail.com>
2023-09-02 19:28:29 +0600

    image modified

--- src/Pages/Home/CustomerReview/ReviewCard.jsx
+++ src/Pages/Home/CustomerReview/ReviewCard.jsx
@@ -4,7 +4,7 @@ const ReviewCard = ({reviews}) => {
     const {customerImg, name, review, ratings} = reviews;
     return (
         <div className="card w-96 h-[35rem] m-5 py-5 bg-blue-50 shadow-xl">
-            <figure><img className="w-52 h-52 rounded-full" src={customerImg} alt="Shoes" /></figure>
+            <figure><img className="w-52 h-52 rounded-full" src={customerImg} alt="User" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
                 <p>{review}</p>
--- src/Pages/Home/Gallery/GalleryCard.jsx
+++ src/Pages/Home/Gallery/GalleryCard.jsx
@@ -4,7 +4,7 @@ const GalleryCard = ({ image }) => {
     const { img } = image
     return (
         <div className="card m-3 w-96 bg-base-100 shadow-xl">
-            <img className="rounded h-64" src={img} alt="Shoes" />
+            <img className="rounded h-64" src={img} alt="Car image" />
 
 
         </div>
